746c6d41975191b9c6e367d7f4fa4aa75189a17c
2Tough <vainferro@gmail.com>
2022-12-02 15:20:32 +0300

    Added a couple of frames

--- script.js
+++ script.js
@@ -5,11 +5,16 @@ const CANVAS_HEIGTH = canvas.height = 600;
 
 const playerImage = new Image()
 playerImage.src = './images/shadow_dog.png';
-let x = 0
+const spriteWidth = 575;
+const spriteHeight = 523
+let frameX = 0;
+let frameY = 0
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGTH);
-    ctx.fillRect(50,50,100,100);
+    //ctx.fillRect(100,50,100,100);
+    //ctx.drawImage(image, sx,sy,sw,sh,dx,dy,dw,dh)
+    ctx.drawImage(playerImage, 0, 0, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)    
     requestAnimationFrame(animate);
 };
 animate()
